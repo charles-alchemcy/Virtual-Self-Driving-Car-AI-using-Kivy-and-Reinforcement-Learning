71a21b9f63a5717c76cec9e2959a4e65f32c1ae9
charles-alchemcy <CharlesDevAlchemy@gmail.com>
2025-11-14 17:16:23 -0500

    model modify

--- Self_Driving_Car/ai.py
+++ Self_Driving_Car/ai.py
@@ -21,7 +21,7 @@ class Network(nn.Module):
         self.input_size = input_size
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30)
-        self.fc2 = nn.Linear(30, nb_action)
+        self.fc2 = nn.Linear(30, 2 * nb_action)
     
     def forward(self, state):
         x = F.relu(self.fc1(state))
